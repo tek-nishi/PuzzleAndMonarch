9a403175a995c73e249a6f66f6ef488654bef7a3
Nishiyama Nobuyuki <tek.nishi@gmail.com>
2018-09-30 14:46:36 +0900

    Windowsでのローカライズ実装

--- src/OsMsw.cpp
+++ src/OsMsw.cpp
@@ -3,7 +3,9 @@
 //
 
 #include "Defines.hpp"
+#include <cinder/app/App.h>
 #include <string>
+#include <map>
 #include "Os.hpp"
 
 
@@ -11,11 +13,18 @@ namespace ngs { namespace Os {
 
 std::string lang()
 {
-  auto* l = setlocale(LC_ALL, "");
+  auto l = std::string(setlocale(LC_ALL, ""));
 
   DOUT << "locale:" << l << std::endl;
 
-  return l;
+  // FIXME 対応言語ごとに追加する必要がある
+  std::string path{ "en.lang" };
+  std::map<std::string, std::string> tbl{
+    { "Japanese_Japan.932", "jp.lang" }
+  };
+  if (tbl.count(l)) path = tbl.at(l);
+
+  return path;
 }
 
 } }
