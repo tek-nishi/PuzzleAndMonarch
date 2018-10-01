92c43102ee51a43c9285c1deeb41c76bcada0a2f
Nishiyama Nobuyuki <tek.nishi@gmail.com>
2018-10-01 23:36:57 +0900

    モデル修正

--- src/OsMsw.cpp
+++ src/OsMsw.cpp
@@ -6,6 +6,7 @@
 #include <cinder/app/App.h>
 #include <string>
 #include <map>
+#include <clocale>
 #include "Os.hpp"
 
 
@@ -13,14 +14,15 @@ namespace ngs { namespace Os {
 
 std::string lang()
 {
-  auto l = std::string(setlocale(LC_ALL, ""));
+  auto l = std::string(std::setlocale(LC_ALL, ""));
 
   DOUT << "locale:" << l << std::endl;
 
   // FIXME 対応言語ごとに追加する必要がある
   std::string path{ "en.lang" };
   std::map<std::string, std::string> tbl{
-    { "Japanese_Japan.932", "jp.lang" }
+    { "Japanese_Japan.932",                      "jp.lang" },
+    { "Chinese (Traditional)_Hong Kong SAR.950", "zh.lang" },
   };
   if (tbl.count(l)) path = tbl.at(l);
 
