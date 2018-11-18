60b4ee5988cc0f6f8b63e6760349219718721e95
Nishiyama Nobuyuki <tek.nishi@gmail.com>
2018-11-18 14:46:00 +0900

    iPadでのShareレイアウト修正

--- src/Share.mm
+++ src/Share.mm
@@ -5,6 +5,34 @@
 #import "Defines.hpp"
 
 
+// 処理を付け足したいので、元クラスを継承したクラスを定義
+@interface  MyUIActivityViewController: UIActivityViewController
+
+- (void)viewWillTransitionToSize:(CGSize)size
+        withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator;
+
+@end
+
+@implementation MyUIActivityViewController
+
+- (void)viewWillTransitionToSize:(CGSize)size
+        withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
+{
+  [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+
+  // 初回は生成タイミングなのでスルーする
+  if (self.beingPresented) return;
+
+  if ([self respondsToSelector:@selector(popoverPresentationController)])
+  {
+    auto rect = CGRectMake(size.width / 2, size.height - 25, 1, 1);
+    self.popoverPresentationController.sourceRect = rect;
+  }
+}
+
+@end
+
+
 namespace ngs { namespace Share {
 
 bool canPost() noexcept
@@ -23,19 +51,20 @@ static NSString* createString(const std::string& text) noexcept
 
   
 void post(const std::string& text, UIImage* image,
-          std::function<void (bool completed)> complete_callback) noexcept {
-
+          std::function<void (bool completed)> complete_callback) noexcept
+{
   NSString* str = createString(text);
 
   // FIXME:もっと賢いNSArrayの構築方法があると思う...
   NSArray* activity_items = image ? @[str, image]
                                   : @[str];
   
-  UIActivityViewController* view_controller = [[[UIActivityViewController alloc] initWithActivityItems:activity_items
-                                                                                 applicationActivities:@[]]
-                                                  autorelease];
+  MyUIActivityViewController* view_controller = [[[MyUIActivityViewController alloc] initWithActivityItems:activity_items
+                                                     applicationActivities:@[]]
+                                                    autorelease];
 
-  void (^completion)(NSString* activity_type, BOOL completed, NSArray *returnedItems, NSError *activityError) = ^(NSString* activity_type, BOOL completed, NSArray *returnedItems, NSError *activityError) {
+  void (^completion)(NSString* activity_type, BOOL completed, NSArray *returnedItems, NSError *activityError) = ^(NSString* activity_type, BOOL completed, NSArray *returnedItems, NSError *activityError)
+  {
     DOUT << "Share::post: completion." << std::endl;
     complete_callback(activityError == nil && completed);
   };
@@ -43,11 +72,16 @@ void post(const std::string& text, UIImage* image,
   
   UIViewController* app_vc = ci::app::getWindow()->getNativeViewController();
 
-  if ([view_controller respondsToSelector:@selector(popoverPresentationController)]) {
+  if ([view_controller respondsToSelector:@selector(popoverPresentationController)])
+  {
     // iPadではpopoverとして表示
-    view_controller.popoverPresentationController.sourceView = app_vc.view;
+    auto* view = app_vc.view;
+    view_controller.popoverPresentationController.sourceView = view;
+    auto bounds = app_vc.view.bounds;
+    auto rect = CGRectMake(bounds.size.width / 2, bounds.size.height - 25, 1, 1);
+    view_controller.popoverPresentationController.sourceRect = rect;
   }
-  
+
   [app_vc presentViewController:view_controller animated:YES completion:nil];
 }
 
